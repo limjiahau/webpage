92daecc5d6cc98f97ed3c85cc58aa775330be209
Joseph Lim <65124287+limjiahau@users.noreply.github.com>
2023-01-05 18:08:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -174,7 +174,7 @@ with st.container():
     st.write("##")
 
     contact_form = """
-    <form action="https://formsubmit.co/josephlimjiahau@gmail.com" method="POST">
+    <form action="https://formsubmit.co/josephlimjiahau@gmail.com" method="POST" />
         <input type="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Your name" required>
         <input type="email" name="email" placeholder="Your email" required>
